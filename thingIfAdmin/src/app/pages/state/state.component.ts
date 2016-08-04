f6565bf7164b4bb0bc2dca0bf7d38a182ed5b274
rizakii <riza.alaudin@kii.com>
2016-08-04 11:46:30 +0900

    States Pages.

--- thingIfAdmin/src/app/pages/state/state.component.ts
+++ thingIfAdmin/src/app/pages/state/state.component.ts
@@ -1,56 +1,79 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 import {BaThemeConfigProvider} from '../../theme';
 import {PieChart} from './pieChart';
-import {StateService} from './state.service';
+import {StateService, SmartLightState} from './state.service';
 import {BaCard} from '../../theme/components';
 import {AppManager} from '../../app.manager';
 import {OnboardingResult, MqttEndpoint} from 'thing-if-sdk';
+import {CHART_DIRECTIVES} from 'ng2-charts';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass} from '@angular/common';
 
 @Component({
   selector: 'state',
   encapsulation: ViewEncapsulation.None,
-  directives: [PieChart,BaCard],
+  directives: [CHART_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES, BaCard],
   providers: [StateService],
   styles: [require('./state.scss')],
   template: require('./state.html')
 })
 export class State {
-  
-  public dashboardColors = this._baConfig.get().colors.dashboard;
 
-  public stateList:Array<any>;
-  public newStateText:string = '';
+  power = {
+    description: 'Power',
+    stats: 'ON',
+    icon: 'bulb',
+  }
+  red = {
+    description: 'Red',
+    stats: 0,
+  }
+  green = {
+    description: 'Green',
+    stats: 0,
+  }
+  blue = {
+    description: 'Blue',
+    stats: 0,
+  }
 
-  constructor(private _baConfig:BaThemeConfigProvider, private _stateService:StateService) {
-    this.stateList = this._stateService.getStateList();
 
-    this.stateList.forEach((item) => {
-      item.color = this._getRandomColor();
+  constructor(private _stateService: StateService) {
+    this._stateService.loadState().then((res: SmartLightState) => {
+      this.pieChartData = res.rgb;
+      this.red.stats = res.rgb[0] ;
+      this.green.stats = res.rgb[1] ;
+      this.blue.stats = res.rgb[2] ;
+      this.pieChartType = this.pieChartType === 'doughnut' ? 'pie' : 'doughnut';
     });
   }
 
-  getNotDeleted() {
-    return this.stateList.filter((item:any) => {
-      return !item.deleted
-    })
-  }
+  public pieChartType: string = 'pie';
 
-  addToDoItem($event) {
-
-    if (($event.which === 1 || $event.which === 13) && this.newStateText.trim() != '') {
-
-      this.stateList.unshift({
-        text: this.newStateText,
-        color: this._getRandomColor(),
-      });
-      this.newStateText = '';
+  // Pie
+  public pieChartLabels: string[] = ['Red', 'Green', 'Blue'];
+  public backgroundColor: Array<any> = [
+    { // red
+      backgroundColor: ['rgba(255,0,0,0.8)', 'rgba(0,255,0,0.8)', 'rgba(0,0,255,0.8)'],
+      borderColor: 'rgba(0,0,0,0)'
     }
+  ];
+  public pieChartData: number[] = [255, 255, 255];
+
+  public randomizeType(): void {
+    this._stateService.loadState().then((res: SmartLightState) => {
+      this.pieChartData = res.rgb;
+
+      this.pieChartType = this.pieChartType === 'doughnut' ? 'pie' : 'doughnut';
+    });
+
+
   }
 
-  private _getRandomColor() {
-    let colors = Object.keys(this.dashboardColors).map(key => this.dashboardColors[key]);
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
 
-    var i = Math.floor(Math.random() * (colors.length - 1));
-    return colors[i];
+  public chartHovered(e: any): void {
+    console.log(e);
   }
 }
--- thingIfAdmin/src/app/pages/state/state.service.ts
+++ thingIfAdmin/src/app/pages/state/state.service.ts
@@ -1,8 +1,17 @@
 import {Injectable} from '@angular/core';
+import * as ThingIFSDK from 'thing-if-sdk';
+import {OnboardingResult} from 'thing-if-sdk'
+import {AppManager} from '../../app.manager';
 
+export class SmartLightState {
+  public power = true;
+  public rgb = [100,200,255];
+}
 @Injectable()
 export class StateService {
+  constructor() {
 
+  }
   private _stateList = [
     { text: 'Check me out' },
     { text: 'Lorem ipsum dolor sit amet, possit denique oportere at his, etiam corpora deseruisse te pro' },
@@ -15,7 +24,19 @@ export class StateService {
     { text: 'Have fun with blur admin' },
     { text: 'What do you think?' },
   ];
+  loadState(): Promise<SmartLightState> {
+    let manager = new AppManager();
+    if (manager.onboardingResult != null && manager.onboardingResult != undefined) {
 
+      return manager.apiAuthor.getState(manager.getTargetID());
+    } else {
+      return new Promise<SmartLightState>((resolve) => {
+        let defaultState =  new SmartLightState();
+        resolve(defaultState);
+      })
+    }
+
+  }
   getStateList() {
     return this._stateList;
   }
