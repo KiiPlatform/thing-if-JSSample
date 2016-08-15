405d33d0207ca7589daf3772c2038225bb444d8f
rizakii <riza.alaudin@kii.com>
2016-08-15 18:54:22 +0900

    add host url

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,13 @@
 
 var kii = require('kii-cloud-sdk').create();
-kii.Kii.initializeWithSite("___APPID___", "___APPKEY___", kii.KiiSite.US);
+/*
+US   : https://api.kii.com
+JP   : https://jp-api.kii.com
+SG   : https://sg-api.kii.com
+CN3  : https://cn3-api.kii.com
+EU   : https://eu-api.kii.com
+*/
+const APP_HOST_URL = '___APP_SITE__';
+kii.Kii.initializeWithSite("___APPID___", "___APPKEY___", APP_HOST_URL+'/api');
 
-export  { kii }
+export  { kii, APP_HOST_URL}
